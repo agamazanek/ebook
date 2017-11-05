ce80866521159569bd157fc37f691c5d88fbfa25
mikolajmarek <mikolaj.juras@gmail.com>
2017-11-05 09:57:52 +0100

    wersja a i b

--- src/mj/test/Main.java
+++ src/mj/test/Main.java
@@ -1,8 +1,25 @@
 package mj.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // 2017-11-05 zad 1 program arguments
+        try {
+
+//            for (int i = 0; i < args.length; i++) {
+//                System.out.println(args[i]);
+//            System.out.println(Arrays.toString(args));
+            System.out.println(args[0]);
+            
+        } catch( ArrayIndexOutOfBoundsException e)
+
+        {
+            System.out.println(e);
+        }
+
     }
 }
