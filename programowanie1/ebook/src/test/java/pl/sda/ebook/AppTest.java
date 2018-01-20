6a43250e7066a4e553509c06b1230da0ee252d3f
mikolajmarek <mikolaj.juras@gmail.com>
2018-01-20 12:42:04 +0100

    init

---
+++ programowanie1/ebook/src/test/java/pl/sda/ebook/AppTest.java
@@ -0,0 +1,14 @@
+package pl.sda.ebook;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
