a4037a4f53519ea5619cd3a294e315c374f8b480
mikolajmarek <mikolaj.juras@gmail.com>
2018-01-20 14:59:26 +0100

    first test should create user

---
+++ programowanie1/ebook/src/test/java/pl/sda/ebook/Registration/RegistrationControllerTest.java
@@ -0,0 +1,36 @@
+package pl.sda.ebook.Registration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RegistrationControllerTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void shouldCreateNewUser() {
+
+        Response loginResult = new RegistrationController().createUser("testUser1", "passw1");
+
+        Assert.assertEquals(true, loginResult.isSuccess());
+
+
+    }
+    /**
+     * data OK & user not exits
+     * Response { success: true }
+     *
+     * data OK & user exits
+     * Response { success: false, msg: User already exist }
+     *
+     * data not OK
+     * Response { success: false, msg: Password too short }
+     */
+
+}
+
