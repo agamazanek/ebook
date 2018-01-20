41525afeac851978e1f9f5808f5fa03512e39182
mikolajmarek <mikolaj.juras@gmail.com>
2018-01-20 16:07:52 +0100

    baza uzytkownikow

---
+++ programowanie1/ebook/src/main/java/UserData/UserDataBase.java
@@ -0,0 +1,24 @@
+package UserData;
+
+import java.util.ArrayList;
+
+public class UserDataBase {
+ ArrayList  <User> users  =new ArrayList<User>();
+
+ public ArrayList<User> getUsers() {
+  return users;
+ }
+
+ public void setUsers(ArrayList<User> users) {
+  this.users = users;
+ }
+
+ public UserDataBase() {
+  this.users=users;
+
+ }
+
+ public Object add(User user) {
+  return null;
+ }
+}
